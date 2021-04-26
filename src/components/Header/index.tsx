5508abc94849c36eacbc5dd497a39748986bb42e
shift1425 <shift1425@gmail.com>
2021-04-26 14:40:15 -0400

    first commit - add dark-mode

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,42 @@
+import format from "date-fns/format";
+import ptBR from "date-fns/locale/pt-BR";
+import styles from "./styles.module.scss";
+import React, { useState } from "react";
+import Switch from "react-switch";
+import Link from "next/link";
+
+export function Header() {
+  const [checked, setChecked] = useState(false);
+  const handleChange = (nextChecked) => {
+    setChecked(nextChecked);
+    if (nextChecked) {
+      document.getElementById("podcastr").className = "dark-mode";
+    } else {
+      document.getElementById("podcastr").className = "";
+    }
+  };
+  const currentDate = format(new Date(), "EEEEEE, d MMMM", {
+    locale: ptBR,
+  });
+  return (
+    <header className={styles.headerContainer}>
+      <Link href={"/"}>
+        <img src="/logo.svg" alt="Podcastr" />
+      </Link>
+      <p>O melhor para você ouvir, sempre</p>
+
+      <span>{currentDate}</span>
+      <div className={styles.darkModeSwitch}>
+        <span>Dark Mode</span>
+        <Switch
+          onChange={handleChange}
+          checked={checked}
+          height={20}
+          width={50}
+          onColor={"#8257E5"}
+          className={styles.switch}
+        ></Switch>
+      </div>
+    </header>
+  );
+}
